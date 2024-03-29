eb81a370fdddc5aff0925c1c20a206ade7b41e04
kperkns2 <kap20k4@gmail.com>
2024-03-29 04:10:22 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import streamlit as st
 token_dictionary = get_token_dictionary()
 
 def on_click():
+  st.write("Processing...")
   inference_job_token = make_initial_request(tts_model_token=speaker_token,
                                             inference_text=text_to_speak)
   if inference_job_token:
-    download_wav_if_complete(inference_job_token)
+    st.write(download_wav_if_complete(inference_job_token))
+    
+    
 
 st.button("Click me!", on_click=on_click)
 
