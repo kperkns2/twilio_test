31a7d9b93a0ec019f280ba9a68b4649770a59de1
kperkns2 <kap20k4@gmail.com>
2024-03-29 04:01:36 +0000

    Update from Google Colab

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from utils import *
 
-
 import streamlit as st
 
-st.write('Hello World2!')
+token_dictionary = get_token_dictionary()
+
+def on_click():
+  inference_job_token = make_initial_request(tts_model_token=speaker_token,
+                                            inference_text=text_to_speak)
+  if inference_job_token:
+    download_wav_if_complete(inference_job_token)
+
+st.button("Click me!", on_click=on_click):
+
+st.write('Hello World!')
+
+name = st.selectbox(list(token_dictionary.keys()))
+speaker_token = token_dictionary[name]
+
+text_to_speak = st.text_input(placeholder='How much wood could a woodchuck chuck if a woodchuck could chuck wood?')
+
+
